50a004e7e0861a2acb12277c722279179ccc52fb
zhangping <zhangping@hosecloud.com>
2021-07-08 18:14:17 +0800

    add appId appSecret

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,19 @@ import { Helmet } from 'react-helmet';
 import '../styles/index.css';
 
 function Index() {
+  const [date, setDate] = useState(null);
+  const [keyInfo, setKeyInfo] = useState({appId: '', appSecret: ''});
   const isSSR = typeof window === "undefined"
+  
   if (!isSSR) {
     console.log('document.referrer', document.referrer);
+    const keyInfoString = localStorage.getItem('KEY_INFO_STRING')
+    if (keyInfoString) {
+      setKeyInfo(JSON.parse(keyInfoString))
+    }
   }
-  const [date, setDate] = useState(null);
+
+
   useEffect(() => {
     async function getDate() {
       const res = await fetch('/api/date');
@@ -16,43 +24,37 @@ function Index() {
     }
     getDate();
   }, []);
+
+  function handleChange (key, value) {
+    console.log('handleChange', key, value, keyInfo);
+    keyInfo[key] = value;
+    setKeyInfo(keyInfo);
+    localStorage.setItem(JSON.stringify(keyInfo))
+  }
+
+  function handleChangeAppId (event) {
+    handleChange('appId', event.target.value);
+  }
+
+  function handleChangeAppSecret (event) {
+    handleChange('appSecret', event.target.value);
+  }
+
   return (
     <main>
       <Helmet>
         <title>Gatsby + Node.js (TypeScript) API</title>
       </Helmet>
       <h1>Gatsby + Node.js (TypeScript) API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/main/examples/gatsby"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        is a <a href="https://www.gatsbyjs.org/">Gatsby</a> app with two
-        directories, <code>/src</code> for static content and <code>/api</code>{' '}
-        which contains a serverless{' '}
-        <a href="https://nodejs.org/en/">Node.js (TypeScript)</a> function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Node.js (TypeScript)
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Node.js (TypeScript) is:</h2>
+      <h2>The date according to Node.js is:</h2>
       <p>{date ? date : 'Loading date...'}</p>
+
+      <input value={keyInfo.appId} onChange={handleChangeAppId} />
+      <p />
+      <input value={keyInfo.appSecret} onChange={handleChangeAppSecret} />
+      <p />
+
+      <input type="button">获取用户信息</input>
     </main>
   );
 }
