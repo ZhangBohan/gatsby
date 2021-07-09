86c41c813cba3224028ba7eddfdd4b68a6022be9
zhangping <zhangping@hosecloud.com>
2021-07-09 10:10:59 +0800

    bug fix

--- api/staff.ts
+++ api/staff.ts
@@ -4,24 +4,30 @@ const axios = require('axios');
 
 
 export default async (req: VercelRequest, res: VercelResponse) => {
-    console.log(req.body);
-    const { baseUrl, appId, appSecret, code } = req.body;
+    try {
+        console.log(req.body);
+        const { baseUrl, appId, appSecret, code } = req.body;
 
-    // 鉴权
-    const tokenRes = await axios.post(baseUrl + 'api/openapi/v1/auth/getAccessToken', {
-        "appKey": appId,
-        "appSecurity": appSecret
-    })
-    console.log('tokenRes', tokenRes.data);
-    const token = tokenRes.data.value.accessToken;
+        // 鉴权
+        const tokenRes = await axios.post(baseUrl + 'api/openapi/v1/auth/getAccessToken', {
+            "appKey": appId,
+            "appSecurity": appSecret
+        })
+        console.log('tokenRes', tokenRes.data);
+        const token = tokenRes.data.value.accessToken;
 
-    const staffRes = await axios({
-        method: 'post',
-        url: `${baseUrl}api/openapi/v3/auth/redirect/${code}`,
-        params: {
-            accessToken: token
-        }
-    })
-    console.log('staffRes', staffRes.data);
-    res.status(200).send(staffRes.data);
+        const staffRes = await axios({
+            method: 'post',
+            url: `${baseUrl}api/openapi/v3/auth/redirect/${code}`,
+            params: {
+                accessToken: token
+            }
+        })
+        console.log('staffRes', staffRes.data);
+        res.status(200).send(staffRes.data);
+    } catch (e) {
+        console.error('e', e);
+        res.status(500).send(e);
+    }
+    
 };
